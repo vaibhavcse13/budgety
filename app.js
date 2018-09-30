24661ba005116a886ec238ee790bd4dba6b5722e
Vaibhav Shukla <cse.vaibhavshukla@gmail.com>
2018-09-30 15:16:09 -0700

    Budget calculation part

--- app.js
+++ app.js
@@ -34,7 +34,28 @@ var BudgetController = (function(){
         totals : {
             expense:0,
             income:0
-        }
+        } ,
+        budget : 0 ,
+        percentage : -1  
+    }
+
+    // to calculate the total income and expense 
+    function calculateTotal(type) {
+        data.totals[type] = data.allItems[type].reduce(function(prev , current){
+            return prev  + current.value ; 
+        }, 0);
+    }
+
+    function calculateBudget() {
+        // calculate total income & expense 
+        calculateTotal("income");
+        calculateTotal("expense");
+        // calculate budget income - expense 
+        data.budget =  data.totals.income -  data.totals.expense;
+        //  calculate the percentage of income that we ? spent 
+
+        data.percentage = data.totals.income > 0 ?  Math.round((data.totals.expense / data.totals.income)* 100) : -1;
+
     }
 
     return {
@@ -57,6 +78,15 @@ var BudgetController = (function(){
         },
         testing : function(){
             console.log(data);
+        },
+        calculateBudget:calculateBudget,
+        getBudget : function(){
+            return {
+                budget : data.budget,
+                totalIncome : data.totals.income , 
+                totalExpense : data.totals.expense,
+                percentage : data.percentage
+            }
         }
     }
 
@@ -71,14 +101,19 @@ var UIController = (function(){
         iaddValue  : '.add__value' ,
         aAddBtn  :'.add__btn' ,
         incomeContainer : ".income__list" , 
-        expenseContainer : ".expenses__list"
-    }
+        expenseContainer : ".expenses__list" , 
+        budgetIncome    :  ".budget__income--value" ,
+        budgetExpense : ".budget__expenses--value" ,
+        budget : ".budget__value" ,
+        expPercentage : '.budget__expenses--percentage'
 
+                        
+    }
     function getIp(){
         return {
             type  : document.querySelector(DOMStrings.iaddType).value  ,//  will be either income or expense 
             description : document.querySelector(DOMStrings.iaddDescr).value ,
-            value :  document.querySelector(DOMStrings.iaddValue).value 
+            value :  parseFloat(document.querySelector(DOMStrings.iaddValue).value  , 10)
            }
      
     }
@@ -117,10 +152,37 @@ var UIController = (function(){
         // insert the new HTML element 
         document.querySelector(element).insertAdjacentHTML("beforeend" , html);
     }
+
+    function clearFields(){
+        var fields , fieldsArr ; 
+                // it returns the list not a array 
+        fields = document.querySelectorAll(DOMStrings.iaddValue + "," + DOMStrings.iaddDescr);
+    
+        fieldsArr = Array.prototype.slice.call(fields);
+    
+        fieldsArr.forEach((element , index , array) => {
+            element.value = "";
+        });
+        fields[0].focus();
+    }
+
+    function updateDOM(selector ,prefix , value , suffix){
+        document.querySelector(selector).textContent = prefix + " "  + value + suffix;
+    }
+    function displayBudget(data) {
+        updateDOM(DOMStrings.budgetIncome , "+" , data.totalIncome , "");
+        updateDOM(DOMStrings.budgetExpense , "-" , data.totalExpense , "");
+        updateDOM(DOMStrings.budget , "" , data.budget , "");
+        if(data.percentage !== -1 ){
+            updateDOM(DOMStrings.expPercentage , "" ,  data.percentage , "%" )
+        }
+    }
     return {
         getInput :  getIp ,
         DOMStrings : DOMStrings ,
-        addListItem : addListItem
+        addListItem : addListItem ,
+        clearFields : clearFields,
+        displayBudget : displayBudget
     }
 }());
 
@@ -133,7 +195,6 @@ var controller = (function(budgetCtrl , uiCtrl){
         //  keypress happen on document
         document.addEventListener('keypress' , function(evt){
             if(evt.keyCode === 13 || event.which === 13){
-                console.log("Enter hit");
                 ctrlAddItem();
                 
             }
@@ -141,24 +202,43 @@ var controller = (function(budgetCtrl , uiCtrl){
     }
 
     var init = function() {
-        console.log("Init Called ");
         setupEventListner(); //  calling the setupEventListner at first place 
+        uiCtrl.displayBudget({
+            totalIncome : 0 ,
+            totalExpense : 0 ,
+            budget:0,
+            percentage : 0
+
+        })
+    }
+
+    function updateBudget() {
+        var budget;
+        // 1. Caclulate budget 
+        budgetCtrl.calculateBudget();
+        //2. return the budget 
+        budget = budgetCtrl.getBudget();
+        //3. display to UI 
+        uiCtrl.displayBudget(budget);
     }
     var ctrlAddItem = function(){
         var input  , newItem ;    
 
         // 1. get the filled input 
-          input = uiCtrl.getInput();
-        // 2. add the item to the budget controller 
-         newItem = budgetCtrl.addItem(input.type ,  input.description , input.value);
-         console.log(newItem);
-         budgetCtrl.testing();
-        //3. add the item to UI 
-        uiCtrl.addListItem(newItem , input.type);
-        //4. calculate the budget 
-
-        // 5. display the budget to the ui 
-        console.log("I am executing ");
+        input = uiCtrl.getInput();
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            // 2. add the item to the budget controller 
+            newItem = budgetCtrl.addItem(input.type ,  input.description , input.value);
+            
+            budgetCtrl.testing();
+            //3. add the item to UI 
+            uiCtrl.addListItem(newItem , input.type);
+            // 4.  clear the field 
+            uiCtrl.clearFields();
+            //4. Calculate the budget and display 
+            updateBudget();        
+        }
+        
     }
     
     return {
